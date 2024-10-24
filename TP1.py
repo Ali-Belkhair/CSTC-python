bdbc21fbfbd5bfa8aff4f3ae53c8b1865f531e5f
Belkhair Ali <alibelkhaircontact@gmail.com>
2024-10-24 23:08:45 +0100

    End Exercice1

---
+++ TP1.py
@@ -0,0 +1,20 @@
+#Exercice 1:
+
+A = int(input("Enter the nombere A : "))
+B = int(input("Enter the nombere B : "))
+
+print("\n The addition betweene A and B is : " ,  A+B )
+print("\n The subtraction betweene A and B is : " , A-B )
+print("\n The multiplication betweene A and B is : " , A*B )
+print("\n Division A / B is : " , A/B )
+
+if(A > B):
+    print( f"\n The first number({A}) is greater than to the second number({B})" )
+if(A < B):
+    print( f"\n The first number({A}) is less than to the second number({B})" )
+if(A ==B ):
+    print( f"\n The first number({A}) is equal to the second number({B})" )
+
+
+#Exercice 2:
+
