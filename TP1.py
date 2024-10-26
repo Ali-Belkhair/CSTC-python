8cf4491b38c40fc612b88c511ae9cc5d6f7cb2fe
Belkhair Ali <alibelkhaircontact@gmail.com>
2024-10-26 23:27:31 +0100

    End Exercice 3

--- TP1.py
+++ TP1.py
@@ -1,4 +1,4 @@
-#Exercice 1:
+#Exercise 1: Basic Data Types and Operators
 
 # A = int(input("Enter the nombere A : "))
 # B = int(input("Enter the nombere B : "))
@@ -15,15 +15,37 @@
 # if(A ==B ):
 #     print( f"\n The first number({A}) is equal to the second number({B})" )
 
-#Exercice 2:
+#Exercise 2: Control Flow
 
-x = int(input("Enter a number : "))
+# x = int(input("Enter a number : "))
 
-if( (x%3)==0 and (x%5)==0 ):
-    print("FizzBuzz")
-elif( (x%3)==0 ):
-    print("Fizz")
-elif( (x%5)==0 ):
-    print("Buzz")
-else:
-    print(x)
+# if( (x%3)==0 and (x%5)==0 ):
+#     print("FizzBuzz")
+# elif( (x%3)==0 ):
+#     print("Fizz")
+# elif( (x%5)==0 ):
+#     print("Buzz")
+# else:
+#     print(x)
+
+
+# Exercise 3: Lists and Loops
+
+L = [12,99,34,53,28,76,65,93,22,1]
+print(L)
+
+sumList = 0
+for i in L :
+    sumList = sumList + i 
+print("The sum of all numbers in the list is : ", sumList )
+
+print("The maximum values in the list : " , max(L) )
+print("The minimum values in the list : " , min(L) )
+
+newL = []
+for i in L :
+    if( i%2 == 0 ):
+        newL.insert(1,i)
+print("The even numbers is : ", newL)
+
+
